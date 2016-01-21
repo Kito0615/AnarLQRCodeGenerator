0f0dfa71c7d2078ce51f9d94c4fd10c33fb8e5d5
Kito0615 <anar0615@sina.com>
2016-01-21 16:17:12 +0800

    Add note

--- AnarLQRCodeGenerator/NSImage+QRCode.h
+++ AnarLQRCodeGenerator/NSImage+QRCode.h
@@ -8,6 +8,9 @@
 
 #import <Cocoa/Cocoa.h>
 
+/**
+ *  颜色结构体，用来表示二维码的前景色和背景色
+ */
 typedef struct QRCodeColor
 {
     CGFloat red;
@@ -17,11 +20,48 @@ typedef struct QRCodeColor
 
 @interface NSImage (QRCode)
 
+/**
+ *  文本生成二维码
+ *
+ *  @param text 文本
+ *
+ *  @return 二维码图片
+ */
 + (NSImage *)QRCodeWithText:(NSString *)text;
 
+/**
+ *  文本生成指定大小的二维码
+ *
+ *  @param text 文本
+ *  @param size 二维码大小
+ *
+ *  @return 二维码图片
+ */
 + (NSImage *)QRCodeWithText:(NSString *)text size:(CGFloat)size;
 
+/**
+ *  文本生成指定大小和前、背景颜色的二维码
+ *
+ *  @param text            文本
+ *  @param size            二维码大小
+ *  @param foreColor       前景色
+ *  @param backgroundColor 背景色
+ *
+ *  @return 二维码图片
+ */
 + (NSImage *)QRCodeWithText:(NSString *)text QRCodeSize:(CGFloat)size QRCodeForeColor:(QRCodeForeColor)foreColor QRCodeBackgroundColor:(QRCodeBackgroundColor)backgroundColor;
+
+/**
+ *  文本和图标生成指定大小和前、背景颜色的二维码
+ *
+ *  @param text            文本
+ *  @param size            二维码大小
+ *  @param foreColor       前景色
+ *  @param backgroundColor 背景色
+ *  @param icon            图标
+ *
+ *  @return 二维码图片
+ */
 + (NSImage *)QRCodeWithText:(NSString *)text QRCodeSize:(CGFloat)size QRCodeForeColor:(QRCodeForeColor)foreColor QRCodeBackgroundColor:(QRCodeBackgroundColor)backgroundColor userIcon:(NSImage *)icon;
 
 @end
